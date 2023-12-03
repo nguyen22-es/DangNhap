515a589cf32bdef903d67ae01d2e6da239b0cc6b
nguyen22-es <112994817+nguyen22-es@users.noreply.github.com>
2023-12-03 09:26:16 +0700

    Add project files.

---
+++ clientDangNhap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace clientDangNhap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
